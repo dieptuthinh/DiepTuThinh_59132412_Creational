0d17fa7cdbb81449955b658b16c6734504d3d036
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2020-05-10 20:30:58 +0700

    chinh sua bai 1

--- BaiTap1/src/App.java
+++ BaiTap1/src/App.java
@@ -2,13 +2,21 @@ import java.text.SimpleDateFormat;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HoaDon hoaDon = new HoaDon.HoaDonBuilder()
+        HoaDon hoaDon1 = new HoaDon.HoaDonBuilder()
                         .setTTC(new HoaDonHeader("HD01",new SimpleDateFormat("dd/mm/yyyy").parse("5/10/2020"),"Phạm Văn Niên"))
                         .addTTCT(new CTHD("Đồng hồ Rolex",1,100000000, 10))
                         .addTTCT(new CTHD("Giày Gucci",1,1000000000, 10))
                         .addTTCT(new CTHD("Bao tay cá sấu",1,200000000, 10))
                         .build();
-        System.out.println(hoaDon.toString());
+        System.out.println(hoaDon1.toString() + "\n");
+
+        HoaDon hoaDon2 = new HoaDon.HoaDonBuilder()
+                        .setTTC(new HoaDonHeader("HD02",new SimpleDateFormat("dd/mm/yyyy").parse("6/10/2020"),"Châu Bá Thông"))
+                        .addTTCT(new CTHD("Áo Superme",10,100000000, 10))
+                        .addTTCT(new CTHD("Giày Gucci",1,1000000000, 10))
+                        .addTTCT(new CTHD("Quần jean",20,200000000, 10))
+                        .build();
+        System.out.println(hoaDon2.toString()+ "\n");
 
     }
 }
