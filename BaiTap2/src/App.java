6826075f03125122b1af4bb6b5c449711c59f7ea
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2020-05-11 07:16:58 +0700

    ham main bai tap 2

--- BaiTap2/src/App.java
+++ BaiTap2/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MyStringBuilder str = new MyStringBuilder("Nhà em ")
+        .addBool(true)
+        .addString(" có ")
+        .addFloat(1f)
+        .addString(" ");
+        str.addString("em trai");
+
+        System.out.println(str.toString());
+        System.out.println(str);
     }
 }
---
+++ BaiTap2/src/MyStringBuilder.java
@@ -0,0 +1,31 @@
+public class MyStringBuilder {
+    String str;
+
+    public MyStringBuilder(String str) {
+        this.str = str;
+    }
+    
+    public MyStringBuilder addString(String s) {
+        str += s;
+        return this;
+    }
+    
+    public MyStringBuilder addFloat(float f) {
+        str += f;
+        return this;
+    }
+    
+    public MyStringBuilder addBool(boolean b) {
+        str += b;
+        return this;
+    }
+    
+   public MyStringBuilder build() {
+       return this;
+   }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+}
